db840d221d7877e74a5e52716f94d0c12bf37cc7
ItsManueh <47420352+ItsManueh@users.noreply.github.com>
2022-04-18 18:10:06 +0200

    Upload fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+--#### FX Information ####--
+fx_version   'cerulean'
+use_fxv2_oal 'yes'
+lua54        'yes'
+game         'gta5'
+
+--#### Resource Information ####--
+name         'Discord Presence Fivem'
+version      '1.0'
+description  'Powered By UraniumProject'
+
+client_script 'client.lua'
+
+server_script 'server.lua'
