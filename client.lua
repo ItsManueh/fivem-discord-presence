c44df22c89817b7c541d72ff6ec070dd8ec3245c
ItsManueh <47420352+ItsManueh@users.noreply.github.com>
2022-04-19 01:24:10 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -6,22 +6,22 @@ end)
 
 CreateThread(function()
 	while true do
-		local player = PlayerPedId()
+	local player = PlayerPedId()
         local playerId = GetPlayerServerId(PlayerId())
 
-		--Updates status
-		Wait(1500)
+	--Updates status
+	Wait(1500)
 
         -- This is the Application ID (Replace this with you own)
-		SetDiscordAppId(YourID)
+	SetDiscordAppId(YourID)
 
-		-- Here you will get the actual numbers of players real time and id players.
+	-- Here you will get the actual numbers of players real time and id players.
         TriggerServerEvent('GetActivePlayers')
 
         SetRichPresence(string.format("%s | %s %s", 'ID: '..playerId..' ' ,playerCount, "Players"))
         
         -- Here you will have to put the image name for the "large" icon.
-		SetDiscordRichPresenceAsset('YourIconLarge')
+	SetDiscordRichPresenceAsset('YourIconLarge')
         
         -- Here you can add hover text for the "large" icon.
         SetDiscordRichPresenceAssetText('Text Sample')
@@ -32,9 +32,9 @@ CreateThread(function()
         -- Here you can add hover text for the "small" icon and get actual name of streets and coords.
         SetDiscordRichPresenceAssetSmallText("Actually in " .. GetStreetNameFromHashKey( GetStreetNameAtCoord ( table.unpack( GetEntityCoords(player)))))     
 
-		--Here you can add buttons that will display in your Discord Status.
+	--Here you can add buttons that will display in your Discord Status.
         SetDiscordRichPresenceAction(0, "First Button!", "TEXT SAMPLE")
         SetDiscordRichPresenceAction(1, "Second Button!", "TEXT SAMPLE")
-		Wait(60000)
+	Wait(60000)
     end
 end)
